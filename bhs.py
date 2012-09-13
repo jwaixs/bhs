e6badb0b8763990ddedf5861d9ed12497d22bf90
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2012-09-13 13:46:55 +0200

    Added new functions: * BasicHypergeometricSeries * littleqJacobiPolynomials * ClebshGordanCoefficients (for SUq(2))

--- bhs.py
+++ bhs.py
@@ -1,5 +1,6 @@
 from sage.symbolic.function import BuiltinFunction, is_inexact 
 
+
 class qPochhammer(BuiltinFunction):
     
     def __init__(self, m=1, expand=False):
@@ -61,14 +62,12 @@ class BasicHypergeometricSeries(BuiltinFunction):
                                  latex_name='{}_{%i}\\phi_{%i}' % (m, n))
 
     def _latex_(self):
-        if self.has_arguments:
-            pass
         return '{}_{%i}\\phi_{%i}' % (self.m, self.n)
-    
+
     def expand_bhs(self, k=0):
-        coef = self._expand_coefficient(k)
+        coef = self.expand_coefficient(k)
         if coef != 0:
-            return coef*self.z**k + self._expand(k+1)
+            return coef*self.z**k + self.expand_bhs(k+1)
         return 0
 
     def expand_coefficient(self, k):
@@ -77,9 +76,9 @@ class BasicHypergeometricSeries(BuiltinFunction):
             return 0
 
         denom = qPochhammer(len(self.list_b)).evaluate(self.list_b, self.q, k)
-        
+     
         coef = nom / denom * \
-            ((-1)**k) * q**(binomial(n, 2))**(len(self.list_b) - len(self.list_a))
+            ((-1)**k * q**(binomial(k, 2)))**(len(self.list_b) - len(self.list_a))
 
         return coef
 
@@ -95,13 +94,156 @@ class BasicHypergeometricSeries(BuiltinFunction):
         self.has_arguments = True
 
         if self.expand:
-            return self._expand()
+            return self.expand_bhs()
         return None
 
     def _evalf_(self, *args, **kwds):
-        return self._expand()
+        return self.expand_bhs()
 
-print "Small test file"
-a, b, q, z = var('a b q z')
-bhs1 = BasicHypergeometricSeries(2, 1, expand=False)
-bhs2 = bhs1(q**(-2), a, b, q, z)
+
+## q-Orthogonal polynomials
+class littleqJacobiPolynomials(BuiltinFunction):
+
+    def __init__(self, expand=True):
+        self.expand = expand
+        self.has_arguments = False
+
+        BuiltinFunction.__init__(self, 'lqjp', nargs=5, latex_name="p")
+
+    def _eval_(self, *args):
+        if len(args) != 5:
+            raise RuntimeError, args
+        
+        self.x = args[0]
+        self.a = args[1]
+        self.b = args[2]
+        self.q = args[3]
+        self.n = args[4]
+
+        self.has_arguments = True
+
+        self.bhs = BasicHypergeometricSeries(2,1,expand=self.expand)
+        if self.expand:
+            return self.bhs(self.q**(-self.n), self.a*self.b*self.q**(self.n+1), \
+                            self.a*self.q, self.q, self.q*self.x)
+  
+        return None
+
+    def general(self, n):
+        x, a, b, q = var('x a b q')
+        return self._eval_(x, a, b, q, n)
+
+## Clebsch-Gordan coefficients
+class ClebschGordanCoefficients():
+    '''Clebsch-Gordan Coefficients for SU_q(2):
+    (i)   l_1 - l_2 \leq j \leq l_2 - l_1 \leq l \leq l_1 + l_2
+    (ii)  l_2 - l_1 \leq j \leq l_1 - l_2 \leq l \leq l_1 + l_2
+    (iii) j \leq l_1 - l_2 \leq -j \leq l \leq l_1 + l_2
+    (iv)  -j \leq l_1 - l_2 \leq j \leq l \leq l_1 + l_2'''
+
+    def __init__(self, q, l1, l2, l, j1, j2, j, expand=True):
+        self.l1 = l1
+        self.l2 = l2
+        self.l = l
+        self.q = q
+        self.j1 = j1
+        self.j2 = j2
+        self.j = j
+        self.expand = expand
+
+    def __repr__(self):
+        return 'C_{%s}(%s, %s, %s; %s, %s, %s)' % (self.q, self.l1, self.l2, self.l, \
+                                                   self.j1, self.j2, self.j)
+
+    def _latex_(self):
+        return 'C^{%s, %s, %s}_{%s; %s %s %s}' % (self.l1, self.l2, self.l, self.q, \
+                                                  self.j1, self.j2, self.j)
+
+    def transform1(self):
+        '''Transform: (i) <=> (iii) and (ii) <=> (iv)'''
+        l1, l2, j1, j2 = self.l1, self.l2, self.j1, self.j2
+        
+        self.l1 = 1/2*(l1 + l2 - self.j)
+        self.l2 = 1/2*(l1 + l2 + self.j)
+        self.j1 = 1/2(-l1 + l2 + j1 - j2)
+        self.j2 = 1/2*(-l1 + l2 - j1 + j2)
+        self.j = l2 - l1
+
+    def transform2(self):
+        '''Transform: (i) <=> (ii) and (iii) <=> (iv)'''
+        l1, l2, j1, j2 = self.l1, self.l2, self.j1, self.j2
+        
+        self.l1 = l2
+        self.l2 = l1
+        self.j1 = -j2
+        self.j2 = -j1
+        self.j  = -self.j
+
+    def currentClass(self):
+        l1, l2, l, j1, j2, j = self.l1, self.l2, self.l, self.j1, self.j2, self.j
+
+        if abs(j1) > l1 or abs(j2) > l2 or l > l1+l2:
+            return 0    # No class
+
+        if l1-l2 <= j and j <= l2-l1 and l2-l1 <= l:
+            return 1
+        elif l2-l1 <= j and j <= l1-l2 and l1-l2 <= l:
+            return 2
+        elif j <= l1-l2 and l1-l2 <= -j and -j <= l:
+            return 3
+        elif -j <= l1-l2 and l1-l2 <= j and j <= l:
+            return 4
+
+        return 0
+
+    def transform2one(self):
+        classnr = self.currentClass()
+        if classnr == 2:
+            self.transform2()
+        elif classnr == 3:
+            self.transform1()
+        elif classnr == 4:
+            self.transform2()
+            self.transform1()
+        elif classnr == 0:
+            return 0
+    
+    def evaluate(self):
+        qpoch = qPochhammer(1, expand=False)
+        bhs   = BasicHypergeometricSeries(3,2,expand=False)
+        l1, l2, l, q, j1, j2, j = self.l1, self.l2, self.l, self.q, \
+                                  self.j1, self.j2, self.j
+
+        # line 1 + end of line 3
+        sign = (-1)**(2*l1-l2+l+j1) * q**(-2*l1*(2*l1+1)-l2+l+j1-(l2-l1-j)*(l+l1+j-j1))
+        qpoch1 = qpoch(q**2, q**2, 2*l1)
+        qpoch2 = qpoch(q**2, q**2, -l1+l2-j)
+        line1 = sign*qpoch1/qpoch2
+
+        # line 2
+        qpoch3 = qpoch(q**(-2*(1+2*l)), q**2, 1)
+        qpoch4 = qpoch(q**(-2), q**(-2), l2-j2)
+        qpoch5 = qpoch(q**(-2), q**(-2), l2+j2)
+        qpoch6 = qpoch(q**(-2), q**(-2), l1+l2-l)
+        qpoch7 = qpoch(q**(-2), q**(-2), l1+l2+l+1)
+        qpoch8 = qpoch(q**(-2), q**(-2), l1-j1)
+        qpoch9 = qpoch(q**(-2), q**(-2), l1+j1)
+        line2 = sqrt(qpoch3*qpoch4*qpoch5/(qpoch6*qpoch7*qpoch8*qpoch9))
+
+        # line 3
+        qpoch10 = qpoch(q**2, q**2, l-l1+l2)
+        qpoch11 = qpoch(q**2, q**2, l-j)
+        qpoch12 = qpoch(q**2, q**2, l+l1-l2)
+        qpoch13 = qpoch(q**2, q**2, l+j)
+        line3 = sqrt(qpoch10*qpoch11/(qpoch12*qpoch13))
+
+        # line 4
+        line4 = bhs(q**(-2*(l1-l2+l)), q**(2*(l-l1+l2+1)), q**(-2*(l1+j1)), \
+                    q**(2*(l2-l1-j+1)), q**(-4*l1), q**2, q**(-2*(l2+j2)))
+        
+        
+        return line1*line2*line3*line4
+
+# Test values
+l1, l2, l, q, j1, j2, j = var('l1 l2 l q j1 j2 j')
+C = ClebschGordanCoefficients(q, l1, l2, l, j1, j2, j)
